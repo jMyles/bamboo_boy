7f81b7f448cefac651a7bf7a28b1272e2b12c003
jMyles <justin@justinholmes.com>
2014-07-10 12:26:45 -0400

    first stalk.

---
+++ README.md
@@ -0,0 +1,3 @@
+Bamboo Boy is a way to share setup and teardown logic between tests, deployments, and development environments.
+
+It's is "Bamboo" because it's a lightweight scaffolding, and "Boy" because it plays nice with factory_boy.
---
+++ bamboo_boy/utils/__init__.py
@@ -0,0 +1,59 @@
+
+class WrongObjectForState(TypeError):
+    pass
+
+def require_state(state_class):
+    '''
+    Can decorate a function or class.
+
+    For <class>, builds state_class and adds it as <class>.test_bed.
+
+    For a function, builds state_class and adds it as first argument.
+
+    (Thus, obviously raises TypeError if function doesn't take any positional arguments.
+    If the function does take a positional argument and the author forgets that it will
+    be a state_class, unpredictable results may ensue.)
+    '''
+    def closure(obj):
+        '''
+        A closure containing the state function with the decorated object in scope.
+        '''
+        def state_run(*args, **kwargs):
+            '''
+            Runs the state, returning the object.
+
+            For TestCases, also runs setUp() and tearDown()
+            '''
+            state = state_class()
+            if hasattr(obj, 'setUp'):  # IE, a TestCase
+                old_setUp = obj.setUp
+                def new_setup(*args, **kwargs):
+                    state.build()
+                    old_setUp(*args, **kwargs)
+
+
+                old_tearDown = obj.tearDown
+                def new_teardown(*args, **kwargs):
+                    state.destroy()
+                    old_tearDown(*args, **kwargs)
+
+                obj.setUp = new_setup
+                obj.tearDown = new_teardown
+
+                obj.test_bed = state
+
+                return obj
+
+            else:
+                def new_func():
+                    test_bed = state.build()
+                    try:
+                        obj(state, *args, **kwargs)
+                    except TypeError:
+                        raise WrongObjectForState("Please pass a TestCase or a function that takes test_bed as its first argument.  This decorator is not appropriate for other objects.")
+                    state.destroy()
+                return new_func
+        return state_run()
+    return closure
+
+
